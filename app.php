99cf7ef4a13b02e1edecddb6aaad81ffa056a12d
whosaidnicole <whosaidnicole@greeny.cs.tlu.ee>
2016-02-18 11:43:05 +0200

    added FROM

--- app.php
+++ app.php
@@ -4,8 +4,9 @@
 	// if there is ?name= .. then $_GET["name"]
 	$my_message = $_GET["message"];
 	$name = $_GET["name"];
+	$from = $_GET["from"];
 
-	echo "My message is ".$my_message. " for ".$name;
+	echo $from." says \"".$my_message."\" to ".$name;
 
 ?>
 
@@ -16,8 +17,12 @@
 	<label for="name">To:<label><br>
 	<input type="text" name="name"><br>
 	
+	<label for="from">From:<label><br>
+	<input type="text" name="from"><br>
+	
 	<label for="message">Message:<label><br>
 	<input type="text" name="message"><br>
+	
 	
 	<input type="submit" value="Save to DB">
 	
